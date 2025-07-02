a73e69cf2d16b549dade666c73c239feb8f201b2
PatrykKups <pkups@anaconda.com>
2025-07-02 09:08:18 +0200

    PKG-8648: aws-c-mqtt 0.12.2 (#3)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,19 @@
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
-cmake -G "Ninja" ^
+cmake -GNinja ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_INSTALL_LIBDIR=lib ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DBUILD_SHARED_LIBS=ON ^
+      -DBUILD_TESTING=ON ^
+      -DCMAKE_POSITION_INDEPENDENT_CODE=ON ^
       ..
 if errorlevel 1 exit 1
 
-ninja install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
-ninja test
+ctest --output-on-failure
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -10,9 +10,11 @@ cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
+  -DBUILD_TESTING=ON \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   ..
-ninja install
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
-  ninja test || (cat Testing/Temporary/LastTest.log && exit 1)
-fi
+
+cmake --build . --config Release --target install
+
+ctest --output-on-failure -j${CPU_COUNT}
 popd
